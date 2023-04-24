f2a65024caf6b9c6cd9b275410e89b16d3f0df4f
dincydominic <dincydominic21@gmail.com>
2023-04-24 12:21:13 +0530

    admin dashboard using angularmetrail and highchart

---
+++ src/app/area-chart/area-chart.component.ts
@@ -0,0 +1,67 @@
+import { Component } from '@angular/core';
+import * as Highcharts from 'highcharts';
+import HC_exporting from 'highcharts/modules/exporting';
+
+@Component({
+  selector: 'app-area-chart',
+  templateUrl: './area-chart.component.html',
+  styleUrls: ['./area-chart.component.css']
+})
+export class AreaChartComponent {
+  Highcharts=Highcharts
+  chartOptions={
+    
+  }
+  constructor()
+  {
+    //2d chart configration
+    this.chartOptions={
+      chart: {
+        renderTo: 'container',
+        type: 'column',
+        options3d: {
+            enabled: true,
+            alpha: 15,
+            beta: 15,
+            depth: 50,
+            viewDistance: 25
+        }
+    },
+    xAxis: {
+        categories: ['MEARN', 'PYTHON', 'DATA SCIENCE', 'TESTING', 'FLUTTER', '.NET',
+            'BIG DATA', 'JAVA SPRING', 'AI', 'ML']
+    },
+    yAxis: {
+        title: {
+            enabled: false
+        }
+    },
+    tooltip: {
+        headerFormat: '<b>{point.key}</b><br>',
+        pointFormat: 'Cars sold: {point.y}'
+    },
+    title: {
+        text: ' YEARLY PLACEMENT ANALYSIS 2022-2023',
+        align: 'center'
+    },
+    subtitle: {
+        text: 'LUMINAR TECHNOLAB',
+        align: 'center'
+    },
+    legend: {
+        enabled: false
+    },
+    plotOptions: {
+        column: {
+            depth: 25
+        }
+    },
+    series: [{
+        data: [1318, 1073, 1060, 813, 775, 745, 537, 444, 416, 395],
+        colorByPoint: true
+    }]
+    }
+    //to export chart
+    HC_exporting(Highcharts);
+  }
+}
